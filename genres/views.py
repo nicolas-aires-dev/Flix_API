b88ad926a681590deb605dc1c0f92322e175e7d9
Nicolas <ni_colasaires@hotmail.com>
2026-01-16 16:54:15 -0300

    Adicionado arquivo permissions.py no /app, contendo permissões globais, e incluído em todas as views as GlobalPermissions

--- genres/views.py
+++ genres/views.py
@@ -1,14 +1,16 @@
 from genres.models import Genre
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
-from  genres.serializers import GenreSerializer
+from genres.serializers import GenreSerializer
+from app.permissions import GlobalDefaultPermission
+
 
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)    
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)    
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 class GenreRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)    
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)    
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ReviewCreateListView.as_view(), name= 'review-create-list' ),
-    path('/int:pk>/', views.ReviewRetrieveUpdateDestroyView.as_view(), name= 'review-detail-update-delete'),
+    path('<int:pk>/', views.ReviewRetrieveUpdateDestroyView.as_view(), name= 'review-detail-update-delete'),
     ]
