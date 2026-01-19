49a1b38db853b02d2676cb46dd8c25523196b077
Nicolas <ni_colasaires@hotmail.com>
2026-01-19 11:31:32 -0300

    implementado endpoint de estatisticas do app 'movie', que lsita: total de filmes, quantos filmes em cada genero, total de reviews e a média de estrelas do banco de filmes

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.MovieCreateListView.as_view(), name= 'movie-create-list' ),
     path('<int:pk>/', views.MovieRetrieveUpdateDestroyView.as_view(), name= 'movie-detail-update-delete'),
+    path('stats/', views.MovieStatsView.as_view(), name= 'movie-stats-view'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics
+from django.db.models import Count, Avg
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movie
 from movies.serializer import MovieSerializer
 from app.permissions import GlobalDefaultPermission
+from reviews.models import Review
 
 class MovieCreateListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)    
@@ -13,3 +15,23 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)    
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset =  Movie.objects.all()
+
+    def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+                                            #campo^^^   ^^^ coluna
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+
+        return response.Response(data=
+                                 {'total_movies': total_movies,
+                                'movies_by_genre': movies_by_genre,
+                                'total_reviews': total_reviews,
+                                'average_stars': average_stars if average_stars else 0
+                                },
+                            status= status.HTTP_200_OK,
+        )        
